80265206194ff51aead7c7a4afe61d05fd439e26
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2024-03-12 08:24:01 +0000

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
+import React, { Component } from "react";  // Corrected import statement
 
-import React from "react";
-import './../styles/App.css';
+class Greetings extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: "" };
+    }
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    onchangeText = (event) => {  // Modified method to accept event parameter
+        const newValue = event.target.value;
+        this.setState({ value: newValue });
+    }
+
+    render() {
+        return (
+            <div>
+                <label htmlFor="input">Enter your name:</label>
+                <input id="input" value={this.state.value} onChange={this.onchangeText} />
+                {this.state.value ? <p>{this.state.value} !</p> : null}
+            </div>
+        );
+    }
 }
 
-export default App
+export default Greetings;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import Greetings from "./components/App";
 
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Greetings />, document.getElementById("root"));
