7dc2b1e780610db2382f110c4dc928aaf81f30d2
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2024-04-16 04:53:32 +0000

    css modified

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";  // Corrected import statement
-import "./App.css";
+import './../styles/App.css';
 class Greetings extends Component {
     constructor(props) {
         super(props);
